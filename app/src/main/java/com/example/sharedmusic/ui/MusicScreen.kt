c40fa77097ec99cf9e47fe4885b0aca3d8e2e36e
amc3y <albertlazarenko345@gmail.com>
2026-09-14 01:56:51 +0300

    Music
    Каждая музыка сохраняется по отдельности с нормальным названием, если оно есть

--- app/src/main/java/com/example/sharedmusic/ui/MusicScreen.kt
+++ app/src/main/java/com/example/sharedmusic/ui/MusicScreen.kt
@@ -1,7 +1,7 @@
 package com.example.sharedmusic.ui
 
+import android.content.Context
 import android.net.Uri
-import android.service.autofill.OnClickAction
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
@@ -9,8 +9,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.runtime.saveable.rememberSaveable
 import com.example.sharedmusic.player.MusicPlayer
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+
+
 
 @Composable
 fun MusicScreen() {
@@ -21,17 +25,17 @@ fun MusicScreen() {
         MusicPlayer(context)
     }
 
-    //выбираем песню
-    var selectMusic by rememberSaveable {
-        mutableStateOf<Uri?>(null)
+    //выбираем песнИ
+    var musicList by remember {
+        mutableStateOf<List<Uri>>(emptyList())
     }
 
     //система выбора файла
     var musicPicker =
         rememberLauncherForActivityResult(
-            ActivityResultContracts.GetContent()
-        ) { uri ->
-            selectMusic = uri
+            ActivityResultContracts.OpenMultipleDocuments()
+        ) { uris ->
+            musicList = musicList + uris
         }
     DisposableEffect(Unit) {
         onDispose {
@@ -42,31 +46,58 @@ fun MusicScreen() {
     Column {
         Button(
             onClick = {
-                musicPicker.launch("audio/*")
+                musicPicker.launch(arrayOf("audio/*"))
             }
         ) {
             Text("Добавить музыку")
         }
 
-        Button(
-            onClick = {
-                selectMusic?.let { uri ->
-                    musicPlayer.play(uri)
+        LazyColumn() {
+            items(musicList) { uri ->
+                Button(
+                    onClick = {
+                        musicPlayer.play(uri)
+                    }
+                ){
+                    Text(
+                        text = getFileName(context, uri)
+                    )
                 }
             }
-        ){
-            Text("Послушать")
         }
 
         Button(
             onClick = {
-                selectMusic?.let { uri ->
-                    musicPlayer.stop(uri)
+                musicPlayer.stop()
                 }
-            }
-
         ) {
             Text("Стоп")
         }
     }
+}
+
+//Функция для нормальных названий файлов
+fun getFileName(context: Context, uri: Uri): String {
+    var fileName = "Неизвестная песня"
+
+    val cursor = context.contentResolver.query(
+        uri,
+        null,
+        null,
+        null,
+        null
+    )
+
+    cursor?.use {
+        if (it.moveToFirst()) {
+            val nameIndex =
+                it.getColumnIndex(android.provider.OpenableColumns.DISPLAY_NAME)
+
+            if (nameIndex >= 0) {
+                fileName = it.getString(nameIndex)
+            }
+        }
+    }
+
+    return fileName
 }
